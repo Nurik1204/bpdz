8779727a7caffad8d90484c1e4d980340f7b219e
Nurik1204 <nurmuhamedchynybekovich@gmail.com>
2023-04-11 14:33:10 +0600

    first

--- dz_2/app.js
+++ dz_2/app.js
@@ -1,23 +1,23 @@
-var Starbucks = {
+const Starbucks = {
     drinks: {
         coffee: {
             americano: "Классический кофе на основе эспрессо с большим количеством горячей воды",
-            latte: "Кофе с молоком",
+            latte: false ,
             cappuccino: "Классический кофе на основе эспрессо с добавлением молочной пены",  },
         tea: {
-            black: "Черный чай",
+            black: false,
             green: "Зеленый чай",
             herbal: "Травяной чай",  },
     }
 };
 
-var drinks = prompt('Какое напиток желаете?')
+const drink = prompt('Какое напиток желаете?')
 
-if (drinks in Starbucks.coffee.latte || drinks in Starbucks.tea.herbal){
+if (drink in Starbucks.drinks.coffee && Starbucks.drinks.coffee[drink]==false ||
+    drink in Starbucks.drinks.tea && Starbucks.drinks.tea[drink]==false ){
     console.log("Выберите что-то другое")
-}else if (drinks in Starbucks.coffee.americano || drinks in Starbucks.coffee.cappuccino){
-    console.log("Ваш заказ принят")
-}else if (drinks in Starbucks.tea.black || drinks in Starbucks.tea.green){
+}else if (drink in Starbucks.drinks.coffee && Starbucks.drinks.coffee[drink]!=false ||
+    drink in Starbucks.drinks.tea && Starbucks.drinks.tea[drink]!=false ){
     console.log('Ваш заказ принят')
 }else {
     console.error('error')
