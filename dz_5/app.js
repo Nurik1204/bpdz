1964e445657a46039d51e4319860fc9f11c963b1
Nurik1204 <nurmuhamedchynybekovich@gmail.com>
2023-04-22 00:12:18 +0600

    dz5

---
+++ dz_5/app.js
@@ -0,0 +1,16 @@
+function reverseString(str){
+    var newString = "«фронтенд»,«апрель»,«урок»";
+    for (var i = str.length - 1; i >= 0; i--){
+        newString += str[i]
+    }
+    return  newString;
+}
+reverseString("«фронтенд»,«апрель»,«урок»")
+
+
+// дз-2
+const getAverage = (numbers) => {
+    const sum = numbers.reduce((acc, number) => acc + number, 0);
+    const length = numbers.length;
+    return sum / length;
+}
